e11e58d67a15dfb2b3bb71d4a318f73ee14c0466
SASCHADDD <saschadanu23@gmail.com>
2025-11-28 20:09:45 +0700

    membuat ui untuk form

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,20 @@
+package com.example.pertemuan9.view.route
+
+interface DestinasiNavigasi {
+    interface DestinasiNavigasi {
+
+        /**
+         * Nama unik untuk menentukan jalur untuk composable
+         */
+
+        val route: String
+
+        /**
+         * String resource id yang berisi judul yang akan ditampilkan di layar
+        halaman.
+         */
+
+        val titleRes: Int
+    }
+
+}

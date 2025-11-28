5d975c8f6148a734cb877e858d164576b1a311e4
SASCHADDD <saschadanu23@gmail.com>
2025-11-28 15:32:52 +0700

    companion object

--- app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -1,4 +1,16 @@
 package com.example.pertemuan9.room
 
-class DatabaseSiswa {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao()  : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+    }
 }

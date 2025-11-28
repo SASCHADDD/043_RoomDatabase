7ad51f25ac5b410c58cd3e5ed1178a3acff2a362
SASCHADDD <saschadanu23@gmail.com>
2025-11-28 20:18:29 +0700

    memperbaiki destinasi home

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.pertemuan9.view.route
 
-object DestinasiHome {
+import com.example.pertemuan9.R
+
+object DestinasiHome : DestinasiNavigasi.DestinasiNavigasi {
+        override val route = "home"
+        override val titleRes = R.string.app_name
 }

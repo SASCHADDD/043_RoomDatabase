6b034e6889bf8358ac1888a26043353894c8aae0
SASCHADDD <saschadanu23@gmail.com>
2025-12-05 18:38:53 +0700

    commit

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.pertemuan9.view.route
 
 import com.example.pertemuan9.R
 
-object DestinasiEntry : DestinasiNavigasi.DestinasiNavigasi{
+object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }

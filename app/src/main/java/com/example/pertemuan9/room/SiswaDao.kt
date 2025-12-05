5efc3843a5e309a83c7bb9d4b2f8084e8b62758b
SASCHADDD <saschadanu23@gmail.com>
2025-12-05 14:45:53 +0700

    menambah query menampilkan data melewati id

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -14,4 +14,7 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
 }
